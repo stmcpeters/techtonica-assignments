c85e89744a18dc1506eb9504174ba1e7299ffb7b
stmcpeters <stmcpeters@gmail.com>
2024-07-19 14:34:08 -0700

    adds ingredients and cooking instructions

---
+++ Techtonica-projects/recipe-page/index.js
@@ -0,0 +1,13 @@
+/*
+let titleHeader = document.querySelector('.title-header');
+
+let newTitleHeader = titleHeader.querySelector('h1');
+newTitleHeader.innerHTML = "hello";
+
+
+
+let ingredientsList = document.querySelector('.ingredients-list');
+
+ingredientsList.innerHTML = "Potatoes, Meat, Dough";
+document.querySelector('ul').append(ingredientsList);
+*/
