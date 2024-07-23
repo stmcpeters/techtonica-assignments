3971bc12dce5e1d9214c68d178b22d5bdb301d5d
stmcpeters <stmcpeters@gmail.com>
2024-07-23 14:08:37 -0700

    undoes DOM test text

--- Techtonica-projects/recipe-page/index.js
+++ Techtonica-projects/recipe-page/index.js
@@ -1,19 +1,8 @@
 /*
-let titleHeader = document.querySelector('.title-header');
-
-let newTitleHeader = titleHeader.querySelector('h1');
-newTitleHeader.innerHTML = "hello";
-
-
-
-let ingredientsList = document.querySelector('.ingredients-list');
-
-ingredientsList.innerHTML = "Potatoes, Meat, Dough";
-document.querySelector('ul').append(ingredientsList);
-*/
-
+------- creates new list item and stores in variable
 var newListItem = document.createElement('li');
-
+------- update the text of that list item
 newListItem.textContent = 'Jalapenos';
-
+------- adds that list item as a child of existing UL
 document.querySelector('ul').appendChild(newListItem);
+*/
