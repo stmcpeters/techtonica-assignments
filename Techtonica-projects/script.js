e8e3fd4fdcc21f889baca951ffe8c77e3b120a2e
stmcpeters <stmcpeters@gmail.com>
2024-07-23 20:04:43 -0700

    git commit -m "deletes duplicate files"

--- Techtonica-projects/script.js
+++
@@ -1,8 +0,0 @@
-/*
-------- creates new list item and stores in variable
-var newListItem = document.createElement('li');
-------- update the text of that list item
-newListItem.textContent = 'Jalapenos';
-------- adds that list item as a child of existing UL
-document.querySelector('ul').appendChild(newListItem);
-*/
