ce0de370d5de1c82ca734e6b1447e2513bb1abce
stmcpeters <stmcpeters@gmail.com>
2024-07-23 20:27:42 -0700

    git commit -m "fixed typo linking JS to HTML"

---
+++ Techtonica-projects/recipe-page/scripts.js
@@ -0,0 +1,7 @@
+// plans 
+// create checkboxes for ingredients that user can check 
+
+
+// selecting filling ingredients
+const ingredientsHeader = document.getElementById('ingredient-header');
+console.log(ingredientsHeader);
