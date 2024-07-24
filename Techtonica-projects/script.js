2e8e8b4e303cb8dcedac3a51f280fef22dcd62b8
stmcpeters <stmcpeters@gmail.com>
2024-07-23 19:50:55 -0700

    updating JS and CSS links to HTML

---
+++ Techtonica-projects/script.js
@@ -0,0 +1,8 @@
+/*
+------- creates new list item and stores in variable
+var newListItem = document.createElement('li');
+------- update the text of that list item
+newListItem.textContent = 'Jalapenos';
+------- adds that list item as a child of existing UL
+document.querySelector('ul').appendChild(newListItem);
+*/
