f973754bd4dc39288080c3b4a4b6c1780a896b9f
stmcpeters <stmcpeters@gmail.com>
2024-07-23 22:31:22 -0700

    added comments for DOM methods

--- Techtonica-projects/recipe-page/scripts.js
+++ Techtonica-projects/recipe-page/scripts.js
@@ -3,5 +3,37 @@
 
 
 // selecting filling ingredients
-const ingredientsHeader = document.getElementById('ingredient-header');
-console.log(ingredientsHeader);
+// const ingredientsHeader = document.getElementById('ingredient-header');
+// console.log(ingredientsHeader);
+
+// // selecting filling ingredients list items
+// const ingredientsList = document.getElementsByClassName('list-items');
+// console.log(ingredientsList);
+
+// // selecting list items in ingredients list
+// const listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+// queryselector selects by tag name, id, class, etc
+// select specifically ingredients div
+// const ingredients = document.querySelector('div.ingredients');
+// console.log(ingredients);
+
+// changing styles of elements
+// select filling ingredients header and change font-color to green
+// const header = document.querySelector('#ingredient-header');
+// header.style.color = 'green';
+// console.log(header);
+
+// creating new element within existing elements
+//     // select existing unordered list for ingredients 
+//     const ul = document.querySelector('.ingredient-list');
+//     // create new list element within
+//     const li = document.createElement('li');
+//     // modify text of new list item
+//     li.innerText = "Chicken";
+//     // appends new list item to ingredients unordered list
+//     ul.append(li);
+
+
+
