98a446bd501142b1a6050eb4eb34b265c19839c7
stmcpeters <stmcpeters@gmail.com>
2024-07-25 19:51:38 -0700

    adds checkboxes to ingredient list items

--- Techtonica-projects/recipe-page/scripts.js
+++ Techtonica-projects/recipe-page/scripts.js
@@ -35,5 +35,35 @@
 //     // appends new list item to ingredients unordered list
 //     ul.append(li);
 
-// testing branch
 
+
+
+// adding checkbox to ingredients list
+const ingredientsList = document.querySelector(".ingredients-list");
+
+// iterate over each <li> element
+ingredientsList.querySelectorAll('.list-items').forEach(item => {
+
+    // creating checkbox element
+    let checkbox = document.createElement("input");
+        //assign attributes to checkboxes
+        checkbox.type = "checkbox";
+        checkbox.name = "name";
+        checkbox.value = "value";
+        checkbox.id = "id" + item.textContent;
+
+    // creating label for checkbox
+    var label = document.createElement("label");
+    // assigning attributes for the created label tag
+    label.htmlFor = "id" + item.textContent;
+
+    // // appending the created text to the created label tag
+    // label.appendChild(
+    //   document.createTextNode("This creates the label for checkbox.")
+    // );
+
+    // appending the checkbox + label to item
+    item.appendChild(checkbox);
+    item.appendChild(label);
+
+});
