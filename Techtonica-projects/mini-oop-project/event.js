1534118481db2205a97983a89eb59a3fddb3fdd3
stmcpeters <stmcpeters@gmail.com>
2024-08-06 12:05:15 -0700

    added search for eligible tickets

--- Techtonica-projects/mini-oop-project/event.js
+++ Techtonica-projects/mini-oop-project/event.js
@@ -1,33 +1,51 @@
 // console.log('hi'); TESTING JS SCRIPT
 
 // creating event class
+// this keyword is used to refer to that specific instance of the variable
 class Event {
   constructor(eventName, description) {
     this.name = eventName;
     this.description = description;
     this.availableTickets = [];
   }
-// function addAvailableTickets(ticketName, price) and push to available ticket array
+// function addAvailableTickets(ticketName, price) 
+// creates new object called newTicket which holds ticketName and its price
+// push newTicket to availableticket array
   addAvailableTickets(ticketName, price){
     const newTicket = new ticketType(ticketName, price);
     this.availableTickets.push(newTicket);
-  }
+  };
 // function allTickets that returns a string with all ticket types and prices:
 // "all tickets: 1. Orchestra ($300) 2. Mezzanine ($200) 3. Balcony ($100)"
   allTickets(){
-    console.log(`All tickets: 1.${this.type} ($${this.price}) 2.${this.type} ($${this.price}) 3.${this.type} ($${this.price})`);
+    // map will filter thru availabletickets array and print each index (will return element's index number into string), ticketname and prices
+    // join method will join all info together separated using spaces
+    return "All tickets: " + this.availableTickets.map((ticket, index) => `${index + 1}. ${ticket.ticketName} ($${ticket.price})`).join(' ')
+  }
+
+// function searchTickets(low to high price range) 
+// filter will order using specification of between low and high value we set 
+// if no tickets within that range, return none
+// return list of eligible tickets
+  searchTickets(low, high){
+    const eligibleTickets = this.availableTickets.filter((ticket) => ticket.price >= low && ticket.price <= high);
+    if(eligibleTickets.length === 0){
+      return "No tickets available"
+    }
+    return "Eligible tickets: " + eligibleTickets.map((ticket, index) => `${index + 1}. ${ticket.ticketName} ($${ticket.price})`).join(' ')
   }
 }
 
 // creating ticket type class (name, price)
 class ticketType {
-  constructor(ticketType,price){
-    this.type = ticketType;
+  constructor(ticketName,price){
+    this.ticketName = ticketName;
     this.price = price;
   }
 }
 
 // using event class to create a new object and assign values
+// key:value --- eventname:description
 const eventObj = new Event(
   'KLOS Golden Gala',
   'An evening with hollywood vampires'
@@ -37,28 +55,45 @@ const eventObj = new Event(
 const eventObj2 = new Event('Puppy Bowl XVI', 'Meet & Greet Event');
 const eventObj3 = new Event('Destiny\'s Child', 'Reunited for one night only');
 
-// create an empty array;
+// create an empty array to hold all events
 const eventArray = [];
 
-// push newly created objects to an array
+// push newly created objects to event array
   eventArray.push(eventObj,eventObj2,eventObj3);
   // console.log(eventArray);
 
-// iterate through array and append to html
+// iterate through eventarray and append to html
+document.addEventListener('DOMContentLoaded', () => {
 let html = '';
 eventArray.forEach((item) => {
   html += `<li>${item.name} - ${item.description}</li>`;
-  });
+});
   document.querySelector('#event').innerHTML = html;
+});
+
+// iterate thru alltickets function and display all tickets available on html
+document.addEventListener('DOMContentLoaded', () => {
+let html = '';
+eventArray.forEach((event) => {
+  html += `<li>${event.name} - ${event.description} - ${event.allTickets()}</li>`;
+});
+  document.querySelector('#event').innerHTML = html;
+});
 
 // adding tickets to each event
   // event 1
-    eventObj.addAvailableTickets('Human', 299);
-    eventObj,addAvailableTickets('Vampire', 99);
-  // event 2
-    eventObj2.addAvailableTickets('General Admission', 25);
-    eventObj2.addAvailableTickets('Floor Seating', 80);
-  // event 3
-    eventObj3.addAvailableTickets('Orchestra', 300);
-    eventObj3.addAvailableTickets('Mezzanine', 200);
-    eventObj3.addAvailableTickets('Balcony', 100);
+  eventObj.addAvailableTickets('Human', 299);
+  eventObj.addAvailableTickets('Vampire', 99);
+// event 2
+  eventObj2.addAvailableTickets('General Admission', 25);
+  eventObj2.addAvailableTickets('Floor Seating', 80);
+// event 3
+  eventObj3.addAvailableTickets('Orchestra', 300);
+  eventObj3.addAvailableTickets('Mezzanine', 200);
+  eventObj3.addAvailableTickets('Balcony', 100);
+
+
+// testing searchtickets within specified range
+console.log(eventObj.searchTickets(0,100));
+console.log(eventObj2.searchTickets(100,200));
+console.log(eventObj3.searchTickets(100,200));
