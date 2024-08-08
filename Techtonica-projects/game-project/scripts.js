7de50cb684f523cee40fad35e5c74c452b125fa6
stmcpeters <stmcpeters@gmail.com>
2024-08-08 12:31:29 -0700

    adds words and hints array

---
+++ Techtonica-projects/game-project/scripts.js
@@ -0,0 +1,40 @@
+// create array to hold objects of words and hints
+const wordsAndHints = [
+  {word: "hello", hint: "human greeting"},
+  {word: "javascript", hint: "programming language that can manipulate and validate data using HTML and CSS"},
+  {word: "array", hint: "collections of ordered items and have indexes"},
+  {word: "object", hint: "collections of unordered properties; properties serve the same purpose as indexes"}
+]
+
+// variable for word currently displaying
+let displayWord = "";
+
+// process to scramble the words
+
+
+
+// process to display corresponding hint with word
+
+
+
+// validation for input box
+
+
+
+
+// button actions
+
+// check/submit button
+
+
+
+// clear button
+
+
+
+
+// score 
+
+
+
+// reset game
