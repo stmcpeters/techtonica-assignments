c2ab9dcc260a8b89ff830c54b114a334f3c4f1bf
stmcpeters <stmcpeters@gmail.com>
2024-08-15 13:55:25 -0700

    creates component to handle user's choice of cards

--- Techtonica-projects/react-game-project/src/SingleCard.jsx
+++ Techtonica-projects/react-game-project/src/SingleCard.jsx
@@ -1,12 +1,17 @@
 import './SingleCard.css'
 
-export default function SingleCard({card}) {
+export default function SingleCard({ card, handleChoice}) {
+
+  const handleClick = () => {
+    handleChoice(card);
+  }
+
   return (
     <div className="card">
       {/* setting display of front and back of cards */}
         <div>
           <img className="front" src={card.src} alt="front of card" />
-          <img className="back" src='/img/cover.jpg' alt="back of card" />
+          <img className="back" src='/img/cover.jpg' alt="back of card" onClick={handleClick} />
         </div>
     </div>
   )
