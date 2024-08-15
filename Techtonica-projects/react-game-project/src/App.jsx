3da28453521ca8121e7c71bf55a45e0859c504f9
stmcpeters <stmcpeters@gmail.com>
2024-08-15 13:55:25 -0700

    creates component to handle user's choice of cards

--- Techtonica-projects/react-game-project/src/App.jsx
+++ Techtonica-projects/react-game-project/src/App.jsx
@@ -15,16 +15,23 @@ const cardImages= [
 
 
 function App() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
+  // creating and setting initial states
+  // initial state of cards (displaying none until new game button is clicked)
+    const [cards, setCards] = useState([]);
+  // initial state of turns user has taken - starting at zero
+    const [turns, setTurns] = useState(0);
 
-// shuffle cards
+  // creating and setting initial states for user choices 
+    const [choiceOne, setChoiceOne] = useState(null)
+    const [choiceTwo, setChoiceTwo] = useState(null)
+
+  // shuffle cards
   const shuffleCards = () => {
     // creates copy of card images and duplicates so theres 2 of each (to match up)
     const shuffledCards = [...cardImages, ...cardImages]
     // sort/shuffle cards - negative result has original order, positive number will shuffle cards 
       .sort(() => Math.random() - 0.5)
-      // for each card you want to assign a unique ID property
+      // iterating thru cards and for each card you want to assign a random unique ID property
       .map((card) => ({ ...card, id: Math.random()}))
 
       // sets new state as shuffled cards after shuffling
@@ -32,7 +39,17 @@ function App() {
       // sets new state for number of turns user has taken
       setTurns(0);
   }
-  // console.log(cards, turns)   test to see if cards are shuffled in console; not next to each other
+  // console.log(cards, turns)   test to see if cards are shuffled in console (not next to each other)
+
+  // handle user choice
+  const handleChoice = (card) => {
+    // checking if the choice is the first or second
+    // choiceOne's initial state value is null(false)
+    // if choiceOne has no value, we want to update the state passing a chosen card's value
+    // if choiceOne has a value already, we want to update the state of choiceTwo passing a chosen card's value
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
 
   return (
     <div className='App'>
@@ -43,7 +60,11 @@ function App() {
         {/* for every card, run the function SingleCard using unique card ids*/}
         {/* initializing props to be exported from App to SingleCard */}
         {cards.map(card => (
-          <SingleCard  key={card.id} card={card}/>
+          <SingleCard  
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice}
+          />
         ))}
       </div>
     </div>
