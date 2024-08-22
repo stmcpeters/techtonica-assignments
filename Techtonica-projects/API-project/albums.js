ec7075579ce37395fc00bf6d885b809534eabfcb
stmcpeters <stmcpeters@gmail.com>
2024-08-22 14:57:36 -0700

    installs pg pkg

--- Techtonica-projects/API-project/albums.js
+++ Techtonica-projects/API-project/albums.js
@@ -1,107 +1,114 @@
+// data info
+// id -serial
+// title -varchar
+// genre -varchar
+// release year -int
+
+
 const albums = [{
   "id": 1,
   "title": "Late Registration",
   "artist": "Kanye West",
   "genre": "Hip-Hop/Rap",
-  "release year": 2005
+  "release-year": 2005
 },
 {
   "id": 2,
   "title": "B'Day",
   "artist": "Beyoncé",
   "genre": "R&B",
-  "release year": 2006
+  "release-year": 2006
 },
 {
   "id": 3,
   "title": "Good Kid, M.A.A.D City",
   "artist": "Kendrick Lamar",
   "genre": "Hip-Hop/Rap",
-  "release year": 2012
+  "release-year": 2012
 },
 {
   "id": 4,
   "title": "Operation: Doomsday",
   "artist": "MF DOOM",
   "genre": "Hip-Hop/Rap",
-  "release year": 1999
+  "release-year": 1999
 },
 {
   "id": 5,
   "title": "1991",
   "artist": "Azealia Banks",
   "genre": "Hip-Hop/Rap",
-  "release year": 2012
+  "release-year": 2012
 },
 {
   "id": 6,
   "title": "The Genie of the Lamp",
   "artist": "Mac Dre",
   "genre": "Hip-Hop/Rap",
-  "release year": 2004
+  "release-year": 2004
 },
 {
   "id": 7,
   "title": "Pink Friday",
   "artist": "Nicki Minaj",
   "genre": "Hip-Hop/Rap",
-  "release year": 2010
+  "release-year": 2010
 },
 {
   "id": 8,
   "title": "Once Upon a Time",
   "artist": "Donna Summers",
   "genre": "Disco",
-  "release year": 1977
+  "release-year": 1977
 },
 {
   "id": 9,
   "title": "Section.80",
   "artist": "Kendrick Lamar",
   "genre": "Hip-Hop/Rap",
-  "release year": 2011
+  "release-year": 2011
 },
 {
   "id": 10,
   "title": "Cowboy Carter",
   "artist": "Beyoncé",
   "genre": "Country",
-  "release year": 2024
+  "release-year": 2024
 },
 {
   "id": 11,
   "title": "Channel Orange",
   "artist": "Frank Ocean",
   "genre": "R&B",
-  "release year": 2012
+  "release-year": 2012
 },
 {
   "id": 12,
   "title": "Blonde",
   "artist": "Frank Ocean",
   "genre": "R&B",
-  "release year": 2016
+  "release-year": 2016
 },
 {
   "id": 13,
   "title": "But You Caint Use My Phone",
   "artist": "Erykah Badu",
   "genre": "R&B",
-  "release year": 2015
+  "release-year": 2015
 },
 {
   "id": 14,
   "title": "Earth, Wind & Fire",
   "artist": "Earth, Wind & Fire",
   "genre": "Soul",
-  "release year": 1971
+  "release-year": 1971
 },
 {
   "id": 15,
   "title": "La La Means I Love You",
   "artist": "The Delfonics",
   "genre": "Soul",
-  "release year": 1968
+  "release-year": 1968
 },
 ]
 
