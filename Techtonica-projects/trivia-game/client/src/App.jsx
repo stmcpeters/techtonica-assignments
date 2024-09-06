671fe42e3e58f996873e411cb6bc1dd7b7412435
stmcpeters <stmcpeters@gmail.com>
2024-09-05 22:19:55 -0700

    creates game component to fetch API and hold game logic

--- Techtonica-projects/trivia-game/client/src/App.jsx
+++ Techtonica-projects/trivia-game/client/src/App.jsx
@@ -1,39 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import Game from './Game.jsx'
 import './App.css'
 
 function App() {
-  const [trivia, setTrivia] = useState(null)
-
-// fetch API data from backend
-  const fetchTrivia = async () => {
-    try {
-      // fetches trivia data from backend server
-      const response = await fetch('http://localhost:8080/trivia')
-
-      // error handling if problems connecting to backend
-      if(!response.ok){
-        throw new error(`HTTP error status: ${response.status}`);
-      }
-
-    // if no errors, parse response in JSON format
-    const trivia = response.json();
-    setTrivia(trivia);
-  } catch (error) {
-    console.error(`Error fetching trivia: `, error);
-  }
-}
-
-// initial data fetch from API
-useEffect(() => {
-  fetchTrivia()
-}, []);
-
-
-// game logic
-// choosing right/wrong answers
-// track score - guessed correct vs # of total questions
-
-
 
 
 
@@ -42,13 +11,11 @@ useEffect(() => {
     {/* displays title */}
       <h1>Reel Trivia</h1>
       {/* conditionally display instructions/starter screen OR trivia gameplay */}
-      {trivia ? (
-        // display trivia question
-
-        // display trivia answers (in buttons?)
+      {/* {trivia ? (
+        // display trivia questions and option buttons
 
         // loading message to display when fetching data from API
-      ) : <p>BRB..fetching trivia</p>}
+      ) : <p>BRB..fetching trivia</p>} */}
     </>
   )
 }
---
+++ Techtonica-projects/trivia-game/client/src/Game.jsx
@@ -0,0 +1,55 @@
+// import hooks from react
+  import { useState, useEffect } from "react";
+
+// game component to load game, and hold question totals + scores
+  const Game = (props) => {
+
+  // initializes and sets score states
+    const [score, setScore] = useState(0);
+  // initializes and sets question states
+    const [questions, setQuestions] = useState([]);
+
+  // fetch API data from backend
+    const loadData = async () => {
+      try {
+        // fetches trivia data from API (backend server)
+      // const response = await fetch('http://localhost:8080/trivia')
+        //fetches hardcoded trivia data for testing
+        const response = await fetch('http://localhost:8080/api/data')
+
+        // error handling if problems connecting to backend
+        if(!response.ok){
+          throw new error(`HTTP error status: ${response.status}`);
+        }
+
+      // if no errors, parse response in JSON format
+      const data = response.json();
+      // testing to see if data has been collected
+      console.log("Received data: ", data)
+      setQuestions(data.questions);
+    } catch (error) {
+      console.error(`Error fetching trivia: `, error);
+    }
+  }
+
+// load game / fetches questions on page load
+  useEffect(() => {
+    loadData()
+  }, []);
+
+
+  return (
+    <>
+      <div className="Container">
+        {questions.map((question, index) => {
+          return <QuestionCard key={index} question={question} />
+        })}
+      </div>
+    </>
+  )
+}
+
+
+// bonus: disables other buttons in row from being selected (1 at a time)
+
+export default Game
