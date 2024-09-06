72bf94d432375564f5dc8ce047297ad8ed2da903
stmcpeters <stmcpeters@gmail.com>
2024-09-05 20:20:51 -0700

    deletes vite placeholder data

--- Techtonica-projects/trivia-game/client/src/App.jsx
+++ Techtonica-projects/trivia-game/client/src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [trivia, setTrivia] = useState(null)
+
+// fetch API data from backend
+  const fetchTrivia = async () => {
+    try {
+      // fetches trivia data from backend server
+      const response = await fetch('http://localhost:8080/trivia')
+
+      // error handling if problems connecting to backend
+      if(!response.ok){
+        throw new error(`HTTP error status: ${response.status}`);
+      }
+
+    // if no errors, parse response in JSON format
+    const trivia = response.json();
+    setTrivia(trivia);
+  } catch (error) {
+    console.error(`Error fetching trivia: `, error);
+  }
+}
+
+// initial data fetch from API
+useEffect(() => {
+  fetchTrivia()
+}, []);
+
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* displays title */}
+      <h1>Reel Trivia</h1>
+      {/* display play screen or trivia questions/answers */}
+      {trivia ? (
+        <p>Question: {trivia.results[0].question}</p>
+        // loading message to display when fetching data from API
+      ) : <p>BRB..fetching trivia</p>}
     </>
   )
 }
