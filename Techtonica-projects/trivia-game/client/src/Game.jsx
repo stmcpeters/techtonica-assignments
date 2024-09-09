10a9fb14fd6c1e8e37717ddd1d3cef6cdf015677
stmcpeters <stmcpeters@gmail.com>
2024-09-09 09:01:24 -0700

    adds more error handling on front and back end

--- Techtonica-projects/trivia-game/client/src/Game.jsx
+++ Techtonica-projects/trivia-game/client/src/Game.jsx
@@ -13,11 +13,13 @@
   // Initialize and set selectedAnswers state
     const [selectedAnswers, setSelectedAnswers] = useState({});
 
+    const [error, setError] = useState(null);
+
   // fetch API data from backend
     const loadData = async () => {
       try {
         // fetches trivia data from API (backend server)
-        //const response = await fetch('http://localhost:8080/trivia')
+        // const response = await fetch('http://localhost:8080/trivia')
         //fetches hardcoded trivia data for testing
         const response = await fetch('http://localhost:8080/api/data')
 
@@ -31,9 +33,10 @@
       // testing to see if data has been collected
       console.log("Received data: ", data.results);
       // displays data as an empty array if it doesn't work
-      setQuestions(data.results || []);
+      setQuestions(data.results);
     } catch (error) {
       console.error(`Error fetching trivia: `, error);
+      setError(error.message);
     }
   }
 
