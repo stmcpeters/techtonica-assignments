05caf64df5abd426d984d9102c3d6c5fca58f423
stmcpeters <stmcpeters@gmail.com>
2024-09-11 11:18:41 -0700

    changes all template info to database/event info

--- Techtonica-projects/eventonica/client/src/components/Navbar.jsx
+++ Techtonica-projects/eventonica/client/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function MyNavBar(props) {
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Cristina Rodriguez</a>
+            Signed in as: <a href="#login">Steph McPeters-Esparza</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
--- Techtonica-projects/eventonica/server/server.js
+++ Techtonica-projects/eventonica/server/server.js
@@ -26,74 +26,77 @@ app.get('/api', (req, res) => {
     res.send('Hola, from My template ExpressJS with React-Vite');
 });
 
-// // create the get request for students in the endpoint '/api/students'
-// app.get('/events', async (req, res) => {
-//     try {
-//         const { rows: events } = await db.query('SELECT * FROM events');
-//         res.send(events);
-//     } catch (e) {
-//         return res.status(400).json({ e });
-//     }
-// });
+// create the get request for students in the endpoint '/api/students'
+app.get('/api/events', async (req, res) => {
+    try {
+        // const { rows: events } = await db.query('SELECT * FROM events');
+        // res.send(events);
+        const result = await db.query('SELECT * FROM public.events;');
+        res.json(result.rows);
+        console.log('connected to events database');
+    } catch (e) {
+        return res.status(400).json({ e });
+    }
+});
 
-// // create the POST request
-// app.post('/api/students', async (req, res) => {
-//     try {
-//         const newStudent = {
-//             firstname: req.body.firstname,
-//             lastname: req.body.lastname,
-//             iscurrent: req.body.iscurrent
-//         };
-//         //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
-//         const result = await db.query(
-//             'INSERT INTO students(firstname, lastname, is_current) VALUES($1, $2, $3) RETURNING *',
-//             [newStudent.firstname, newStudent.lastname, newStudent.iscurrent],
-//         );
-//         console.log(result.rows[0]);
-//         res.json(result.rows[0]);
+// create the POST request
+app.post('/api/events', async (req, res) => {
+    try {
+        const newEvent = {
+            title: req.body.title,
+            location: req.body.location,
+            eventtime: req.body.eventtime
+        };
+        //console.log([newEvent.title, newEvent.location, newEvent.eventtime]);
+        const result = await db.query(
+            'INSERT INTO events(title, location, eventtime) VALUES($1, $2, $3) RETURNING *',
+            [newEvent.title, newEvent.location, newEvent.eventtime],
+        );
+        console.log(result.rows[0]);
+        res.json(result.rows[0]);
 
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(400).json({ e });
-//     }
+    } catch (e) {
+        console.log(e);
+        return res.status(400).json({ e });
+    }
 
-// });
+});
 
-// // delete request for students
-// app.delete('/api/students/:studentId', async (req, res) => {
-//     try {
-//         const studentId = req.params.studentId;
-//         await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-//         console.log("From the delete request-url", studentId);
-//         res.status(200).end();
-//     } catch (e) {
-//         console.log(e);
-//         return res.status(400).json({ e });
+// delete request for students
+app.delete('/api/events/:eventId', async (req, res) => {
+    try {
+        const eventId = req.params.eventId;
+        await db.query('DELETE FROM events WHERE id=$1', [eventId]);
+        console.log("From the delete request-url", eventId);
+        res.status(200).end();
+    } catch (e) {
+        console.log(e);
+        return res.status(400).json({ e });
 
-//     }
-// });
+    }
+});
 
-// // put request - Update a student 
-// app.put('/api/students/:studentId', async (req, res) =>{
-//     //console.log(req.params);
-//     //This will be the id that I want to find in the DB - the student to be updated
-//     const studentId = req.params.studentId
-//     const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-//     console.log("In the server from the url - the student id", studentId);
-//     console.log("In the server, from the react - the student to be edited", updatedStudent);
-//     // UPDATE students SET lastname = "something" WHERE id="16";
-//     const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-//     const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-//     try {
-//       const updated = await db.query(query, values);
-//       console.log(updated.rows[0]);
-//       res.send(updated.rows[0]);
+// put request - Update a student 
+app.put('/api/events/:eventId', async (req, res) =>{
+    //console.log(req.params);
+    //This will be the id that I want to find in the DB - the student to be updated
+    const eventId = req.params.eventId
+    const updatedEvent = { id: req.body.id, title: req.body.title, lastname: req.body.location, eventtime: req.body.eventtime}
+    console.log("In the server from the url - the student id", studentId);
+    console.log("In the server, from the react - the student to be edited", updatedStudent);
+    // UPDATE students SET location = "something" WHERE id="16";
+    const query = `UPDATE events SET title=$1, location=$2, eventtime=$3 WHERE id=${eventId} RETURNING *`;
+    const values = [updatedEvent.title, updatedEvent.location, updatedEvent.eventtime];
+    try {
+      const updated = await db.query(query, values);
+      console.log(updated.rows[0]);
+      res.send(updated.rows[0]);
   
-//     }catch(e){
-//       console.log(e);
-//       return res.status(400).json({e})
-//     }
-//   })
+    }catch(e){
+      console.log(e);
+      return res.status(400).json({e})
+    }
+  })
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
