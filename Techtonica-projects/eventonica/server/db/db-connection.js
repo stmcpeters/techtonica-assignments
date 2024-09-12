2abb30b6c4a8b6b05685cf876d57565283e6d4ff
stmcpeters <stmcpeters@gmail.com>
2024-09-11 23:38:55 -0700

    adds comments for troubleshooting database connection to eventonica/events database, but was unable. added hardcoded data file and created get and post routes

--- Techtonica-projects/eventonica/server/db/db-connection.js
+++ Techtonica-projects/eventonica/server/db/db-connection.js
@@ -1,7 +1,34 @@
-import pg from 'pg';
-const { Pool } = pg;
+import pkg from 'pg';
+const { Pool } = pkg;
 const db = new Pool({
     connectionString: process.env.DATABASE_URI
   });
 
+  //module.exports = db;
   export default db;
+
+// const { Pool } = require('pg');
+// const db = new Pool({
+//     connectionString: process.env.DATABASE_URI
+//   });
+//   export default db;
+
+
+
+//Import Pool class from pg package to use for connection pooling
+// import pkg from 'pg';
+
+// const { Pool } = pkg;
+
+// //Create db instance of Pool class with secret configs
+// const db = new Pool({
+//     user: process.env.DB_USER,          
+//     host: process.env.DB_HOST,          
+//     database: process.env.DB_NAME,      
+//     password: process.env.DB_PASSWORD,  
+//     port: process.env.DB_PORT,    
+// });
+
+// //Export db object for server to import and use  
+// // module.exports = db;
+// export default db;
---
+++ Techtonica-projects/eventonica/server/hardcodedEvents.js
@@ -0,0 +1,45 @@
+// eventonica database info
+// id -- integer    SERIAL PRIMARY
+// title -- varchar
+// location -- varchar
+// category -- varchar
+// date -- date MM DD YYYY
+
+const hardcodedEvents = [{
+       "id": 1,
+      "title": "Women in Tech Techtonica Panel",
+      "location": "Overland Park Convention Center",
+      "category": "Speaker Series",
+      "date": "04-21-2023"
+    },
+    {
+      "id": 2,
+      "title": "Japanese Cultural Education",
+      "location": "Seattle Convention Center",
+      "category": "Speaker Series",
+      "date": "05-12-2023"
+    },
+    {
+      "id": 3,
+      "title": "Haven 90s Party Night Club",
+      "location": "Hilton Hotel Kansas City",
+      "category": "Night Event",
+      "date": "03-29-2023"
+    },
+    {
+      "id": 4,
+      "title": "Comedy Night at the Station",
+      "location": "SF Hilton Hotel",
+      "category": "Night Event",
+      "date": "03-29-2023"
+    },
+    {
+      "id": 5,
+      "title": "A Decadent Arts Experience",
+      "location": "West Ridge Hall",
+      "category": "Art Exhibit",
+      "date": "05-25-2023"
+    }
+];
+
+export default hardcodedEvents;
